1281e13a43090b439c915090bba430196c1bccaf
Andrew Kim <awkim.11@gmail.com>
2017-02-14 18:05:51 -0500

    Completed exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,17 @@ class Store < ActiveRecord::Base
 
   has_many :employees
 
+  validate :clothing, on: :create
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {
+                               only_integer: true,
+                               greater_than_or_equal_to: 0
+                            }
+  def clothing
+    if (!mens_apparel && !womens_apparel)
+      errors.add(:base, "#{:mens_apparel} and #{:womens_apparel} can't both be false")
+    end
+  end
+
 end
